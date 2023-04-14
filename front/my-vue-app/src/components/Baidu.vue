2e5287ac9d4cee9022e294e29e9b2bb469f1689c
evpeople <hangcaihui@gmail.com>
2023-04-14 21:53:29 +0800

    有红点，能移动

--- front/my-vue-app/src/components/Baidu.vue
+++ front/my-vue-app/src/components/Baidu.vue
@@ -35,58 +35,29 @@ import { usePointGeocoder, PointGeocoderResult } from 'vue3-baidu-map-gl'
 import { BInfoWindow } from 'vue3-baidu-map-gl'
 import { defineComponent } from 'vue'
 import { onMounted } from 'vue'
-import { ImgHTMLAttributes } from 'vue'
 export default defineComponent({
   setup(props, context) {
     // 在这里声明数据，或者编写函数并在这里执行它
     const map = ref()
-    const imgP = ref<ImgHTMLAttributes>()
+    const imgP = ref()
     const { get, result, isLoading, isEmpty } = usePointGeocoder<PointGeocoderResult>()
     const point = ref({ lng: 116.30793520652882, lat: 40.05861561613348 })
+    const markerPoint = point
+    const show2 = ref<boolean>(true)
     const imgBASEURL = "https://api.map.baidu.com/panorama/v2?width=139&height=104&location="
     const lastBaseURL = "&fov=180&ak=Np5CcMkThnumd5L4MpQ01LjMxvrjUyxr"
     var imgURL = ""
     var d = "ds"
-
-
-    const show2 = ref<boolean>(true)
     // onMounted(() => {
-    //   // console.log("before", imgP.value.alt);
+    //   console.log("before", imgP.value.alt);
     //   imgP.value.src = imgURL
     //   imgP.value.alt = d
     //   console.log(imgP.value.alt);
     // })
-
-
-
-    return {
-      map,
-      show2,
-      point,
-      result,
-      isLoading,
-      isEmpty,
-      imgURL,
-      imgBASEURL,
-      d,
-      imgP
-      // 需要给 `<template />` 用的数据或函数，在这里 `return` 出去
-    }
-
-  },
-  methods: {
-
-    // 在这里声明方法
-    handleInitd() {
-      const { get, result, isLoading, isEmpty } = usePointGeocoder<PointGeocoderResult>()
-      const point = ref({ lng: 116.30793520652882, lat: 40.05861561613348 })
+    function handleInitd() {
       get(point.value)
-      console.log("called inited ");
-      console.log(isEmpty.value, isLoading.value);
-
-    },
-    handleClick2(e) {
-      const show2 = ref<boolean>(true)
+    }
+    function handleClick2(e) {
       console.log("hsssadas");
 
       if (show2.value == false) {
@@ -95,16 +66,8 @@ export default defineComponent({
         show2.value = false
 
       }
-    },
-    handleClick(e) {
-      const imgBASEURL = "https://api.map.baidu.com/panorama/v2?width=139&height=104&location="
-      const lastBaseURL = "&fov=180&ak=Np5CcMkThnumd5L4MpQ01LjMxvrjUyxr"
-      var imgURL = ""
-      var d = "ds"
-      const show2 = ref<boolean>(true)
-      const { get, result, isLoading, isEmpty } = usePointGeocoder<PointGeocoderResult>()
-      const point = ref({ lng: 116.30793520652882, lat: 40.05861561613348 })
-      const markerPoint = point
+    }
+    async function handleClick(e) {
       markerPoint.value = e.latlng
       // if (show2.value == false) {
       //   show2.value = true
@@ -128,6 +91,23 @@ export default defineComponent({
 
     }
 
+
+    return {
+      map,
+      handleClick,
+      handleClick2,
+      handleInitd,
+      show2,
+      point,
+      result,
+      isLoading,
+      isEmpty,
+      imgURL,
+      imgBASEURL,
+      d,
+      imgP
+      // 需要给 `<template />` 用的数据或函数，在这里 `return` 出去
+    }
   },
 })
 
